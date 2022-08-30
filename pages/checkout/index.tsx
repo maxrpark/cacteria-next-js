ebc82af41b07b4bc17c347956e62c04435d96f9e
Maxigong <maxi@nolgong.com>
2022-08-30 09:26:15 +0900

    loadStrip

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -7,6 +7,8 @@ import { useCartContext } from "../../context/useCartContext";
 import { NextPage } from "next";
 let stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
+console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
 const CheckoutPage: NextPage = () => {
   const { total_amount, cart } = useCartContext();
   const [clientSecret, setClientSecret] = useState("");
